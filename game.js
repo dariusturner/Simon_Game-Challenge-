6344e67d589f3f09e00349fd8cd17dc93244fd05
Darius Turner <dariusot731@gmail.com>
2019-08-29 21:56:38 -0400

    Create nextSequence function to play sounds and fade buttons - Complete

---
+++ game.js
@@ -0,0 +1,15 @@
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+
+  new Audio("sounds/" + randomChosenColor + ".mp3").play();
+  $("." + randomChosenColor).fadeOut(100);
+  setTimeout(function() {
+    $("." + randomChosenColor).fadeIn(100);
+  }, 50);
+}
