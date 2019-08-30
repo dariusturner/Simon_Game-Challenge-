0c3e8d92b3f4657e446b5b3187b104cc4e1979d0
Darius Turner <dariusot731@gmail.com>
2019-08-30 13:36:56 -0400

    Add functions playSound() and animatePressed() & update h1 with current level

--- game.js
+++ game.js
@@ -1,18 +1,43 @@
+//Global variables
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
+var level = 0;
 
+//Sequence function
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
 
   gamePattern.push(randomChosenColor);
+  $("#level-title").text("Level " + level);
+  level++;
 
-  new Audio("sounds/" + randomChosenColor + ".mp3").play();
+  playSound(randomChosenColor);
   $("." + randomChosenColor).fadeOut(100).fadeIn(100);
 }
 
+//Event Listeners
 $(".btn").click(function() {
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
+  animatePressed(userChosenColor);
+  playSound(userChosenColor);
 });
+
+$($).keydown(function() {
+  nextSequence();
+});
+
+
+//Functions
+function playSound(name) {
+  new Audio("sounds/" + name + ".mp3").play();
+}
+
+function animatePressed(currentColor) {
+  $("." + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
+}
