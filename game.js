561934c420d805d195a94feb2a8367ae1ec5c6fc
Darius Turner <dariusot731@gmail.com>
2019-09-01 13:56:59 -0400

    Start Over and game features - Complete

--- game.js
+++ game.js
@@ -4,18 +4,6 @@ var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 var level = 0;
 
-//Sequence function
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNumber];
-
-  gamePattern.push(randomChosenColor);
-  $("#level-title").text("Level " + level);
-  level++;
-
-  playSound(randomChosenColor);
-  $("." + randomChosenColor).fadeOut(100).fadeIn(100);
-}
 
 //Event Listeners
 $(".btn").click(function() {
@@ -23,6 +11,8 @@ $(".btn").click(function() {
   userClickedPattern.push(userChosenColor);
   animatePressed(userChosenColor);
   playSound(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 $($).keydown(function() {
@@ -40,4 +30,46 @@ function animatePressed(currentColor) {
   setTimeout(function() {
     $("." + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("Correct");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    }, 200);
+
+    // Start Over
+    $("#level-title").text("Game Over, Press Any Key To Restart");
+    startOver();
+  }
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+  $("#level-title").text("Level " + (level + 1));
+  level++;
+
+  playSound(randomChosenColor);
+  $("." + randomChosenColor).fadeOut(100).fadeIn(100);
+}
+
+function startOver() {
+  level = 0;
+  userClickedPattern = [];
+  gamePattern = [];
 }
